f54feb900c2be736107637ab6ad2eac08d9c58df
Shradhaa81 <25ece030.shradhatripathy@giet.edu>
2026-03-31 19:06:27 +0530

    Add files via upload

---
+++ structure.c
@@ -0,0 +1,22 @@
+#include <stdio.h> 
+#include <string.h> 
+struct Student { 
+    int roll; 
+    char name[50]; 
+    int age; 
+}; 
+void display(struct Student s[], int n) { 
+    for (int i = 0; i < n; i++) 
+        if (s[i].age >= 20) 
+            prin ("%d %s %d\n", s[i].roll, s[i].name, s[i].age); 
+} 
+int main() { 
+    struct Student s[5]; 
+    for (int i = 0; i < 5; i++) { 
+        scanf("%d", &s[i].roll); 
+        getchar(); 
+        fgets(s[i].name, 50, stdin); 
+        s[i].name[strcspn(s[i].name, "\n")] = 0; 
+        scanf("%d", &s[i].age); 
+    } 
+    display(s, 5);    return 0;     }
